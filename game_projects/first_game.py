9fce8ca3767b19c422acddd2cb29344db2946f31
Gerrit Fourie <g.fourie@vanlanschotkempen.com>
2026-03-11 08:36:47 +0100

    game development practice

---
+++ game_projects/first_game.py
@@ -0,0 +1,34 @@
+import pygame
+
+# initialize pygame
+pygame.init()
+
+# create the screen
+screen = pygame.display.set_mode((800, 600))
+
+# title and icon
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('ufo.png')
+pygame.display.set_icon(icon)
+
+# player
+playerImg = pygame.image.load('spaceship.png')
+playerX = 370
+playerY = 480
+
+def player():
+    screen.blit(playerImg, (playerX, playerY))
+
+# game loop
+running = True
+
+while running:
+    # RGB - Red, Green, Blue
+    screen.fill((0, 100, 0))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    player()
+    pygame.display.update()
